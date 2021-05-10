8e0b91c984a8e224c0e059d5ec453d206b65c45b
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2021-05-10 10:28:26 -0400

    Fix retrieve cmd

--- manifests/site.pp
+++ manifests/site.pp
@@ -20,12 +20,14 @@ END
 
   $version = '3.1.1'
   psick::netinstall { "spark-v${version}.tar.gz":
-    destination_dir => '/usr/local/spark',
-    url             => "https://mirror.its.dal.ca/apache/spark/spark-${version}/spark-${version}-bin-hadoop3.2.tgz",
-    extract_command => 'tar -zxf',
-    owner           => 'root',
-    group           => 'root',
-    creates         => "/usr/local/spark/spark-${version}-bin-hadoop3.2",
+    destination_dir  => '/usr/local/spark',
+    url              => "https://mirror.its.dal.ca/apache/spark/spark-${version}/spark-${version}-bin-hadoop3.2.tgz",
+    retrieve_command => 'curl',
+    retrieve_args    => '-L -O',
+    extract_command  => 'tar -zxf',
+    owner            => 'root',
+    group            => 'root',
+    creates          => "/usr/local/spark/spark-${version}-bin-hadoop3.2",
   }
 
 }
