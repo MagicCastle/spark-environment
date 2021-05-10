fd9fe2906e62acbee2254cf2714880c388ce897e
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2021-05-10 11:35:37 -0400

    Add master and worker service

--- manifests/site.pp
+++ manifests/site.pp
@@ -6,7 +6,7 @@ class base {
   $instances = lookup('terraform.instances')
   $tags = lookup("terraform.instances.${::hostname}.tags")
 
-$host_template = @(END)
+  $host_template = @(END)
 127.0.0.1 localhost localhost.localdomain localhost4 localhost4.localdomain4
 <% @instances.each do |key, values| -%>
 <%= values['local_ip'] %> <%= key %> <% if values['tags'].include?('puppet') %>puppet<% end %>
@@ -19,7 +19,7 @@ END
   }
 
   $version = '3.1.1'
-  psick::netinstall { "spark-v${version}.tar.gz":
+  psick::netinstall { "spark-${version}":
     destination_dir  => '/opt',
     url              => "https://mirror.its.dal.ca/apache/spark/spark-${version}/spark-${version}-bin-hadoop3.2.tgz",
     retrieve_command => 'curl',
@@ -30,6 +30,85 @@ END
     creates          => "/opt/spark-${version}-bin-hadoop3.2",
   }
 
+  $master_ip = lookup('terraform.tag_ip.master.0')
+  $cores = ${::processorcount}
+  $memory = round(${::memoryfree_mb} / 100) * 100
+  file { '/opt/spark-${version}-bin-hadoop3.2/conf/spark-env.sh':
+    content => @("END")
+#!/usr/bin/env bash
+SPARK_MASTER_HOST=${master_ip}
+SPARK_WORKER_CORES=${cores}
+SPARK_WORKER_MEMORY=${memory}M
+END
+  }
+
+}
+
+class master {
+  $version = '3.1.1'
+
+  file { '/etc/systemd/system/spark-master.service':
+    content => @("END")
+[Unit]
+Description=Apache Spark Master
+After=network.target
+After=systemd-user-sessions.service
+After=network-online.target
+
+[Service]
+User=root
+Type=forking
+ExecStart=/opt/spark-${version}-bin-hadoop3.2/sbin/start-master.sh
+ExecStop=/opt/spark-${version}-bin-hadoop3.2/sbin/stop-master.sh
+TimeoutSec=30
+Restart=on-failure
+RestartSec=30
+StartLimitInterval=350
+StartLimitBurst=10
+
+[Install]
+WantedBy=multi-user.target
+END
+  }
+
+  service { 'spark-master':
+    ensure => running,
+    enable => true,
+    require => File['/etc/systemd/system/spark-master.service']
+  }
+}
+
+class worker {
+  $version = '3.1.1'
+  file { '/etc/systemd/system/spark-worker.service':
+    content => @("END")
+[Unit]
+Description=Apache Spark Worker
+After=network.target
+After=systemd-user-sessions.service
+After=network-online.target
+
+[Service]
+User=root
+Type=forking
+ExecStart=/opt/spark-${version}-bin-hadoop3.2/sbin/start-worker.sh
+ExecStop=/opt/spark-${version}-bin-hadoop3.2/sbin/stop-worker.sh
+TimeoutSec=30
+Restart=on-failure
+RestartSec=30
+StartLimitInterval=350
+StartLimitBurst=10
+
+[Install]
+WantedBy=multi-user.target
+END
+  }
+
+  service { 'spark-worker':
+    ensure => running,
+    enable => true,
+    require => File['/etc/systemd/system/spark-worker.service']
+  }
 }
 
 node default {
