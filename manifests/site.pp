f091d057801251c133f3a17c72d1e1c4cf209c95
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2021-05-10 09:53:53 -0400

    Add netinstall

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,6 +3,15 @@ class base {
   include psick
   package { 'java': }
 
+  $version = '3.1.1'
+  psick::netinstall { "spark-v${version}.tar.gz":
+    destination_dir => '/usr/local/spark',
+    url             => "https://mirror.its.dal.ca/apache/spark/spark-${version}/spark-${version}-bin-hadoop3.2.tgz",
+    extract_command => 'tar -zxf',
+    owner           => 'root',
+    group           => 'root',
+    creates         => "/usr/local/spark/spark-${version}-bin-hadoop3.2",
+  }
 }
 
 node default {
