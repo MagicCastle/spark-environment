186e5784298d48055a5ce6f68382f276bc4642e0
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2021-05-10 10:04:22 -0400

    Add /etc/hosts

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,6 +3,21 @@ class base {
   include psick
   package { 'java': }
 
+  $instances = lookup('terraform.instances')
+  $tags = lookup("terraform.instances.${::hostname}.tags")
+
+$host_template = @(END)
+127.0.0.1 localhost localhost.localdomain localhost4 localhost4.localdomain4
+<% @instances.each do |key, values| -%>
+<%= values['local_ip'] %> <%= key %> <% if values['tags'].include?('puppet') %>puppet<% end %>
+<% end -%>
+END
+
+  file { '/etc/hosts':
+    ensure  => file,
+    content => inline_template($host_template)
+  }
+
   $version = '3.1.1'
   psick::netinstall { "spark-v${version}.tar.gz":
     destination_dir => '/usr/local/spark',
@@ -12,6 +27,7 @@ class base {
     group           => 'root',
     creates         => "/usr/local/spark/spark-${version}-bin-hadoop3.2",
   }
+
 }
 
 node default {
